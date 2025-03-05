bf31efd3be13a4f508971f5554f5f06be73e5468
Ayres Vitor <gitkey@virtuaires.com.br>
2025-03-05 14:56:46 -0300

    address breaking changes

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,10 +1,8 @@
 import { defineCollection } from 'astro:content';
-import { i18nSchema } from '@astrojs/starlight/schema';
-import { docsSchema } from 'src/schemas/docsSchema';
+import { docsSchema, i18nSchema } from "@astrojs/starlight/schema';
+import { docsLoader, i18nLoader } from "@astrojs/starlight/loaders";
 
 export const collections = {
-  docs: defineCollection({
-    schema: docsSchema,
-  }),
-  i18n: defineCollection({ type: 'data', schema: i18nSchema() }),
+  docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
+  i18n: defineCollection({ loader: i18nLoader(), schema: i18nSchema() })
 };
