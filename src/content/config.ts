080f54eb6efc8b99c1f0e6007d62d93096930b29
Ayres Vitor <gitkey@virtuaires.com.br>
2025-03-05 15:25:47 -0300

    Revert "address breaking changes"
    This reverts commit bf31efd3be13a4f508971f5554f5f06be73e5468.

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,8 +1,10 @@
 import { defineCollection } from 'astro:content';
-import { docsSchema, i18nSchema } from "@astrojs/starlight/schema';
-import { docsLoader, i18nLoader } from "@astrojs/starlight/loaders";
+import { i18nSchema } from '@astrojs/starlight/schema';
+import { docsSchema } from 'src/schemas/docsSchema';
 
 export const collections = {
-  docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
-  i18n: defineCollection({ loader: i18nLoader(), schema: i18nSchema() })
+  docs: defineCollection({
+    schema: docsSchema,
+  }),
+  i18n: defineCollection({ type: 'data', schema: i18nSchema() }),
 };
